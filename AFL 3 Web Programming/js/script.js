0ff150f18c3bdfb439438df9761125aafd788f60
Sonnn03 <jsilvester@student.ciputra.ac.id>
2026-04-29 21:31:47 +0800

    third commit

--- AFL 3 Web Programming/js/script.js
+++
@@ -1,9 +0,0 @@
-// // Fungsi untuk mengganti tema dan mengubah teks tombol
-// // Fungsi untuk menampilkan informasi kontak
-function showContact() {
-    alert("Halo! Anda dapat menghubungi saya melalui email: jasonnn@gmail.com  No HP : 081234567890");
-}
-
-function showContact() {
-    alert("Halo!\n\nAnda dapat menghubungi saya melalui:\n- Email: jasonnn@gmail.com\n- No HP: 081234567890\n- Instagram: @jasonnnn.03");
-}
